84d62a9325be2d040d30e8aa67891910cda5cbfd
fsmutimeer <ishpata@hotmail.com>
2021-07-10 15:16:56 +0500

    promotion updated

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Promotions = require('../models/promotions');
+
 const promoRouter = express.Router();
 promoRouter.use(bodyParser.json());
 
 promoRouter.route('/')
-.all((req, res, next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req, res, next)=>{res.end('Will send all the promotions to you!');})
 .post((req, res, next)=>{res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
 })
