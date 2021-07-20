1858d940ff1c433660fbadf2533aef1dfac26bf6
fsmutimeer <ishpata@hotmail.com>
2021-07-20 19:18:38 +0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1684097001777329',
+        clientSecret: 'a194092484afa3be22561ddddd385006'
+    }
 }
